e7742cd301b5fa0d0983183d3195a1c9fa672716
mohamed693 <mohamed.nasr20977@gmail.com>
2023-10-15 08:21:00 +0300

    update index.html: Add reviews feature

---
+++ js/swiper.js
@@ -0,0 +1,29 @@
+var swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  loop: true,
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    640: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+  },
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
